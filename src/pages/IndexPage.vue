c4dc1b783b479756783529227d6f26bce1c7cb16
Camilo-Zapata <camilozgbmediagroup@gmail.com>
2024-02-28 17:00:12 -0500

    init

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,39 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+    <div class="q-pa-md fixed-center"  style="width: 1250px; background-color:black;">
+   
+   
+  <div class="row justify-center">
+   <div class="col-12">
+      <q-item v-ripple>
+            <p style="color:white; font-size:large  " class=" fixed-center" > LA NUEVA VISIÓN DE LA INDUSTRIA TRAE CONSIGO MUCHAS SORPRESAS</p>
+      </q-item>   
+     </div>  
+
+  </div>
+
+   
+    <q-list bordered>
+
+
+     
+      <q-separator />
+
+
+
+  <div class="col-12  " >
+      <q-item>
+          <q-input rounded color="lime-11" bg-color="green" filled v-model="text" label="Label"> </q-input>
+      </q-item>
+        
+  </div>
+     
+
+     
+
+
+     
+    </q-list>
+  </div>
 </template>
 
 <script>
